b14346e2b90754c37d6cc5eb5c6c54ee4da7960b
roniekun <roniebenitez01@gmail.com>
2023-10-25 19:25:02 +0800

    update

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -6,6 +6,16 @@ import Socials from '../../assets/icons/soclals';
 const Footer = ({color, bgColor, setColor}) => {
 
   const date = new Date();
+    // EMAIL CONFIG   // EMAIL CONFIG   // EMAIL CONFIG   // EMAIL CONFIG   // EMAIL CONFIG
+
+    const email = 'roniebenitez01@gmail.com';
+    const subject = 'Project Request';
+  
+    const handleEmailClick = () => {
+      const mailtoUrl = `mailto:${email}?subject=${encodeURIComponent(subject)}`;
+      window.location.href = mailtoUrl;
+    };
+
   return (
     <div
     style={{background: 'black'}}
@@ -19,17 +29,24 @@ const Footer = ({color, bgColor, setColor}) => {
       containerProps={{ gap: '14px'
       }}
       />
-        </div>
-        <div className={styles.socialContainer}>
-          <h4>Socials</h4>
+       </div>
+      <div className={styles.itemContainer}>
+      <div className={styles.socialContainer}>
+          <h4>Let&apos;s Connect </h4>
         <Socials
         containerProps={{gap:'14px', flexWrap: 'wrap'}}
           linkProps={{ color: 'whitesmoke',
                                 textTransform: 'uppercase',
                                 fontSize: '16px'}}
           displayNames={true}/>
-    
       </div>
+      <div className={styles.emailContainer}>
+      <h4>Email Me</h4>
+      <p onClick={handleEmailClick} style={{color: "white"}}>roniebenitez01@gmail.com</p>
+         </div>
+      </div>
+ 
+
       <h5 className={styles.date}>&copy; {date.getFullYear()} All Rights Reserved </h5>
        
     </div>
